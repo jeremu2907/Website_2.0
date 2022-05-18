4ebdb2a1ff1d50bb4418b3bc8bbe747b4867fe71
jeremu2907 <89870941+jeremu2907@users.noreply.github.com>
2022-05-18 15:35:31 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function wait(){
+    //adding a terminal line
+    setTimeout(() => {
+    document.getElementById("disp").innerHTML = "<br><m style='color: #65a450'>me@MyWebSite</m>:<m style='color: #397e9c '>/JEREMY_NGUYEN</m>$ More about me and my projects on my <a href='https://www.linkedin.com/in/jeremu2907/' class='link' target='_blank'>LinkedIn</a> and <a href='https://github.com/jeremu2907' class='link2' target='_blank'>GitHub</a><br>";
+    }, 10000);
+    
+    //Creating new terminal line
+    setTimeout(() => {
+        document.getElementById("animate").innerHTML = "<div class='typing intro'><m style='color: #65a450'>me@MyWebSite</m>:<m style='color: #397e9c '>/JEREMY_NGUYEN</m>$ More about me and my projects on my<a href='https://www.linkedin.com/in/jeremu2907/' class='link' target='_blank'> LinkedIn</a> and <a href='https://github.com/jeremu2907' class='link2' target='_blank'>GitHub</a></div>"
+    },4000);
+
+    setTimeout(() => {
+        document.getElementById("animate").innerHTML = "<div class='typing2 intro'><m style='color: #65a450'>me@MyWebSite</m>:<m style='color: #397e9c ''>/JEREMY_NGUYEN</m>$ Email me at <a href='mailto:jeremu2907@gmail.com? target='_blank'' style='color:rgb(210, 209, 209);'>jeremu2907@gmail.com</a><div>"
+    },10000)
+}
+wait();

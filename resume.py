c531475509c04a37a57f0b1659ea1aaf7e5eeaf6
jeremu2907 <jeremu2907@gmail.com>
2022-08-25 20:18:13 -0500

    'Resumeupdate'

--- resume.py
+++ resume.py
@@ -2,3 +2,6 @@ import shutil
 import os
 
 shutil.copy(open(open(".gitignore", "r").readline(),"r").readline(), os.getcwd())
+os.system("git add .")
+os.system("git commit -m 'Resumeupdate' ")
+os.system("git push")
